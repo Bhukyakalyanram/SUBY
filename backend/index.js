954d2329538e9f44b39515d1ef5c4a86534ff905
Bhukyakalyanram <Kalyanrambhukya54@gmail.com>
2025-09-01 17:10:00 +0530

    second commit

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(
   cors({
     origin: [
       'http://localhost:5173', // for local dev
-      'https://suby-cyan.vercel.app',
+      'https://suby-cyan.vercel.ap',
       'https://suby-8cob.vercel.app',
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
