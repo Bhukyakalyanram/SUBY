07703443d55a1544756cf203be41b9e12f9bceb5
Bhukyakalyanram <Kalyanrambhukya54@gmail.com>
2025-09-01 16:29:27 +0530

    second commit

--- react_dashboard/src/vendorDashboard/data/apiPath.js
+++ react_dashboard/src/vendorDashboard/data/apiPath.js
@@ -1,3 +1,3 @@
 // export const API_URL = 'https://backend-nodejs-suby-ji26.onrender.com';
 
-export const API_URL = 'http://localhost:4000';
+export const API_URL = 'https://suby-kkb7.onrender.com';
